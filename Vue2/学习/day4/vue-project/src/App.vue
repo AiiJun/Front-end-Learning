0cfa1ecf4035128324a3c5d736a370874699e241
AiiJun <137049458+AiiJun@users.noreply.github.com>
2023-10-06 15:32:03 +0800

    v-model原理

--- Vue2/学习/day4/vue-project/src/App.vue
+++ Vue2/学习/day4/vue-project/src/App.vue
@@ -1,50 +1,22 @@
 <template>
   <div class="app">
-    我是APP组件
-    <button @click="change">修改数据</button>
-    <SonA></SonA>
-    <SonB></SonB>
+    <input v-model="msg1" type="text" />
+    <br />
+    <!-- 模板中获取事件的形参 -> $event 获取 -->
+    <input :value="msg2" @input="msg2 = $event.target.value" type="text" >
   </div>
 </template>
 
 <script>
-import SonA from './components/SonA.vue'
-import SonB from './components/SonB.vue'
 export default {
-  provide() {
-    return {
-      // 简单类型 是非响应式的
-      color: this.color,
-      // 复杂类型 是响应式的
-      userInfo: this.userInfo,
-    }
-  },
   data() {
     return {
-      color: 'pink',
-      userInfo: {
-        name: 'zs',
-        age: 18,
-      },
+      msg1: '',
+      msg2: '',
     }
-  },
-  methods: {
-    change() {
-      this.color = 'red'
-      this.userInfo.name = 'ls'
-    },
-  },
-  components: {
-    SonA,
-    SonB,
   },
 }
 </script>
 
 <style>
-.app {
-  border: 3px solid #000;
-  border-radius: 6px;
-  margin: 10px;
-}
 </style>
