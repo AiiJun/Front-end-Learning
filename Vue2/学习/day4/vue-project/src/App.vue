7cfe424091dd0ce0aa12f3b7257dfcb221fcda67
AiiJun <137049458+AiiJun@users.noreply.github.com>
2023-10-06 15:19:56 +0800

    非父子组件-事件总线

--- Vue2/学习/day4/vue-project/src/App.vue
+++ Vue2/学习/day4/vue-project/src/App.vue
@@ -1,74 +1,24 @@
 <template>
-  <!-- 主体区域 -->
-  <section id="app">
-    <!-- 头部区域 -->
-    <iKunHeader @AddItem="Add"></iKunHeader>
-    <!-- 主体区域 -->
-    <iKunMain :list="list" @delById="delItemById"></iKunMain>
-    <!-- 底部区域 -->
-    <iKunFooter :list="list" @clear="clear"></iKunFooter>
-  </section>
+  <div class="app">
+    <BaseA></BaseA>
+    <BaseB></BaseB>
+    <BaseC></BaseC>
+  </div>
 </template>
 
 <script>
-import iKunHeader from "./components/iKunHeader.vue";
-import iKunMain from "./components/iKunMain.vue";
-import iKunFooter from "./components/iKunFooter.vue";
+import BaseA from './components/BaseA.vue'
+import BaseB from './components/BaseB.vue'
+import BaseC from './components/BaseC.vue'
 export default {
-  components: {
-    iKunHeader,
-    iKunMain,
-    iKunFooter
-  },
-  data() {
-    const defaultList = [
-      {
-        id: 1,
-        name: "唱"
-      },
-      {
-        id: 2,
-        name: "跳"
-      },
-      {
-        id: 3,
-        name: "RAP"
-      },
-      {
-        id: 4,
-        name: "篮球"
-      }
-    ];
-    return {
-      list: JSON.parse(localStorage.getItem("list")) || defaultList
-    };
-  },
-  methods: {
-    delItemById(e) {
-      // console.log(e);
-      this.list = e;
-    },
-    Add(name) {
-      // console.log(name);
-      this.list.push({
-        id: +new Date(),
-        name
-      });
-    },
-    clear(e) {
-      this.list = e;
-    }
-  },
-  watch: {
-    list: {
-      deep: true,
-      handler() {
-        localStorage.setItem("list", JSON.stringify(this.list));
-      }
-    }
+  components:{
+    BaseA,
+    BaseB,
+    BaseC
   }
-};
+}
 </script>
 
 <style>
+
 </style>
