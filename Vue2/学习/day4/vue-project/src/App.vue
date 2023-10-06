e332d3ca550705a6f7d0106032c7f5b5cdfe0393
AiiJun <137049458+AiiJun@users.noreply.github.com>
2023-10-06 15:09:40 +0800

    iKun记事本-Vue组件版

--- Vue2/学习/day4/vue-project/src/App.vue
+++ Vue2/学习/day4/vue-project/src/App.vue
@@ -1,23 +1,70 @@
 <template>
-  <div class="app">
-    <BaseCount :count="count" @changeCount="changeCount"></BaseCount>
-  </div>
+  <!-- 主体区域 -->
+  <section id="app">
+    <!-- 头部区域 -->
+    <iKunHeader @AddItem="Add"></iKunHeader>
+    <!-- 主体区域 -->
+    <iKunMain :list="list" @delById="delItemById"></iKunMain>
+    <!-- 底部区域 -->
+    <iKunFooter :list="list" @clear="clear"></iKunFooter>
+  </section>
 </template>
 
 <script>
-import BaseCount from "./components/BaseCount.vue";
+import iKunHeader from "./components/iKunHeader.vue";
+import iKunMain from "./components/iKunMain.vue";
+import iKunFooter from "./components/iKunFooter.vue";
 export default {
   components: {
-    BaseCount
+    iKunHeader,
+    iKunMain,
+    iKunFooter
   },
   data() {
+    const defaultList = [
+      {
+        id: 1,
+        name: "唱"
+      },
+      {
+        id: 2,
+        name: "跳"
+      },
+      {
+        id: 3,
+        name: "RAP"
+      },
+      {
+        id: 4,
+        name: "篮球"
+      }
+    ];
     return {
-      count: 100
+      list: JSON.parse(localStorage.getItem("list")) || defaultList
     };
   },
   methods: {
-    changeCount(e) {
-      this.count = e;
+    delItemById(e) {
+      // console.log(e);
+      this.list = e;
+    },
+    Add(name) {
+      // console.log(name);
+      this.list.push({
+        id: +new Date(),
+        name
+      });
+    },
+    clear(e) {
+      this.list = e;
+    }
+  },
+  watch: {
+    list: {
+      deep: true,
+      handler() {
+        localStorage.setItem("list", JSON.stringify(this.list));
+      }
     }
   }
 };
---
+++ Vue2/学习/day4/vue-project/src/components/iKunHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- 输入框 -->
+  <header class="header">
+    <h1>iKun记事本-组件版</h1>
+    <input placeholder="请输入任务" class="new-todo" v-model="name" @keyup.enter="handleAdd"/>
+    <button class="add" @click="handleAdd">添加任务</button>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ""
+    };
+  },
+  methods:{
+    handleAdd(){
+        // if(!this.name){
+        //     alert('请输入任务')
+        //     return
+        // }
+        if(this.name.trim() === ''){
+            return
+        }
+        this.$emit('AddItem',this.name)
+        this.name = ''
+    }
+  }
+};
+</script>
+
+<style>
+</style>
