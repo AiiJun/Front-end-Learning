224970f03c8291ce556b25c942efb7ddcc4d09e1
AiiJun <137049458+AiiJun@users.noreply.github.com>
2023-10-06 17:37:21 +0800

    day5-Vue异步更新和$nextTick

--- Vue2/学习/day4/vue-project/src/App.vue
+++ Vue2/学习/day4/vue-project/src/App.vue
@@ -1,23 +1,57 @@
 <template>
   <div class="app">
-    <h4>父组件 -- <button @click="handleGet">获取组件实例</button></h4>
-    <BaseForm ref="baseForm"></BaseForm>
+    <div v-if="isShowEdit">
+      <input type="text" v-model.trim="editValue" ref="inp" @keyup.enter="add" />
+      <button @click="add">确认</button>
+      <button @click="back">返回</button>
+    </div>
+
+    <div v-else>
+      <span>{{ title }}</span>
+      <button @click="handleEdit">编辑</button>
+    </div>
   </div>
 </template>
 
 <script>
-import BaseForm from './components/BaseForm.vue'
 export default {
-  components: {
-    BaseForm,
+  data() {
+    return {
+      title: "大标题",
+      isShowEdit: false,
+      editValue: ""
+    };
   },
   methods: {
-    handleGet(){
-      // console.log(this.$refs.baseForm.getFormData());
-      // this.$refs.baseForm.resetFormData()
+    handleEdit() {
+      // 1.显示输入框 (异步dom更新)
+      this.isShowEdit = true;
+      // 2.让输入框获取焦点
+      // console.log(this.$refs.inp); // this.$refs.inp == undefined
+      // this.$refs.inp.focus()
+      this.$nextTick(() => {
+        console.log(this.$refs.inp); // this.$refs.inp == undefined
+        this.$refs.inp.focus();
+      });
+
+      // setTimeout(() => {
+      //   console.log(this.$refs.inp); // this.$refs.inp == undefined
+      //   this.$refs.inp.focus();
+      // }, 1000);
+    },
+    add() {
+      if (this.editValue.trim() === "") {
+        return;
+      }
+      this.title = this.editValue;
+      this.isShowEdit = false;
+      this.editValue = "";
+    },
+    back() {
+      this.isShowEdit = false;
     }
   }
-}
+};
 </script>
 
 <style>
