4de58fb8f371a9b4a59300748d2ad4fcf6e90755
AiiJun <137049458+AiiJun@users.noreply.github.com>
2023-10-08 15:20:45 +0800

    默认插槽与具名插槽

--- Vue2/学习/day5/vue-profile/src/App.vue
+++ Vue2/学习/day5/vue-profile/src/App.vue
@@ -1,157 +1,76 @@
 <template>
-  <div class="app">
-    <!-- <div class="base-chart-box">
-      这是一个捣乱的盒子
-    </div>-->
-    <h1 v-color="color1">自定义指令1</h1>
-    <h1 v-color="color2">自定义指令2</h1>
-    <input type="text" name id ref="inp" v-focus />
-    <div class="box" v-loading="isLoading">
-      <ul>
-        <li v-for="item in list" :key="item.id" class="news">
-          <div class="left">
-            <div class="title">{{ item.title }}</div>
-            <div class="info">
-              <span>{{ item.source }}</span>
-              <span>{{ item.time }}</span>
-            </div>
-          </div>
-
-          <!-- <div class="right">
-            <img :src="item.img" alt="">
-          </div>-->
-        </li>
-      </ul>
-    </div>
-    <BaseChart></BaseChart>
-    <div class="box2" v-loading="isLoading2"></div>
+  <div>
+    <!-- 2.在使用组件时, 组件标签内填入内容 -->
+    <!-- 3. 具名插槽 -->
+    <MyDialog>
+      <!-- 需要template标签包裹起来 -->
+      <template v-slot:head>
+        <div>我是大标题</div>
+      </template>
+      <template #body>
+        <div>我是主体部分</div>
+      </template>
+      <template #foot>
+        <div>
+          <button>确认</button>
+          <button>取消</button>
+        </div>
+      </template>
+    </MyDialog>
+    <MyTable :data="list">
+      <!-- 3.通过template #插槽名="变量名" 接收 -->
+      <template #default="obj">
+        <!-- {{ obj }} -->
+        <button @click="del(obj.row.id)">删除</button>
+      </template>
+    </MyTable>
+    <MyTable :data="list2">
+      <!-- 3.通过template #插槽名="变量名" 接收 -->
+      <template #default="{row}">
+        <!-- {{ obj }} -->
+        <button @click="show(row)">查看</button>
+      </template>
+    </MyTable>
   </div>
 </template>
+
 <script>
-// 安装axios =>  yarn add axios
-import axios from "axios";
-import BaseChart from "./components/BaseChart.vue";
+import MyDialog from "./components/MyDialog.vue";
+import MyTable from "./components/MyTable.vue";
 export default {
-  components: {
-    BaseChart
-  },
-  // mounted() {
-  //   this.$refs.inp.focus();
-  // }
-  async created() {
-    // 1.发送请求获取数据
-    const res = await axios.get("http://hmajax.itheima.net/api/news");
-    // console.log(res);
-    setTimeout(() => {
-      // 2.更新到list中, 用于页面渲染 v-for
-      this.list = res.data.data;
-      this.isLoading = false;
-    }, 2000);
-  },
   data() {
     return {
-      color1: "red",
-      color2: "green",
-      list: [],
-      isLoading: true,
-      isLoading2:true
+      list: [
+        { id: 1, name: "张晓华", age: 18 },
+        { id: 2, name: "孙大明", age: 19 },
+        { id: 3, name: "刘得中", age: 17 }
+      ],
+      list2: [
+        { id: 1, name: "赵小云", age: 18 },
+        { id: 2, name: "刘培培", age: 19 },
+        { id: 3, name: "蒋小琴", age: 17 }
+      ]
     };
   },
-  // 2. 局部注册指令
-  directives: {
-    // 指令名: 指令的配置项
-    focus: {
-      inserted(el) {
-        el.focus();
-      }
+  components: {
+    MyDialog,
+    MyTable
+  },
+  methods: {
+    del(id) {
+      // console.log(id);
+      this.list = this.list.filter(item => item.id != id);
     },
-    color: {
-      // 1. inserted 提供的是元素被添加到页面中时的逻辑
-      inserted(el, binding) {
-        // console.log(el,binding.value);
-        // binding.value 就是指令的值
-        // el.style.color = "red";
-        el.style.color = binding.value;
-      },
-      // 2.update指令的值修改的时候触发, 提供值变化后, dom更新的逻辑
-      update(el, binding) {
-        console.log("指令的值修改了");
-        el.style.color = binding.value;
-      }
-    },
-    loading: {
-      inserted(el, binding) {
-        console.log(binding);
-        binding.value
-          ? el.classList.add("loading")
-          : el.classList.remove("loading");
-      },
-      update(el, binding) {
-        binding.value
-          ? el.classList.add("loading")
-          : el.classList.remove("loading");
-      }
+    show(row) {
+      console.log(row);
+      alert(`姓名:${row.name};年龄:${row.age}`);
     }
   }
 };
 </script>
 
 <style>
-li {
-  list-style: none;
-}
-
-/* 伪类 - 蒙层效果 */
-.loading:before {
-  content: "";
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background: #fff url("./loading.gif") no-repeat center;
-}
-
-.base-chart-box {
-  width: 200px;
-  height: 100px;
-}
-.box {
-  width: 800px;
-  min-height: 500px;
-  border: 3px solid orange;
-  border-radius: 5px;
-  position: relative;
-}
-.news {
-  display: flex;
-  height: 120px;
-  width: 600px;
-  margin: 0 auto;
-  padding: 20px 0;
-  cursor: pointer;
-}
-.news .left {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding-right: 10px;
-}
-.news .left .title {
-  font-size: 20px;
-}
-.news .left .info {
-  color: #999999;
-}
-.news .left .info span {
-  margin-right: 20px;
-}
-
-.box2 {
-  width: 400px;
-  height: 400px;
-  border: 2px solid #000;
-  position: relative;
+body {
+  background-color: #b3b3b3;
 }
 </style>
--- Vue2/学习/day5/vue-profile/src/main.js
+++ Vue2/学习/day5/vue-profile/src/main.js
@@ -1,17 +1,8 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
-
-// // 1.全局注册指令
-// Vue.directive("focus", {
-//   // inserted 会在指令所在的元素, 被插入到页面中时触发
-//   inserted(el) {
-//     // console.log(el);
-//     el.focus()
-//   },
-// });
+Vue.config.productionTip = false
 
 new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
