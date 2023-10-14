abf9e2a2ad6dd3d67b9aa47d15b1b41dd2eba787
AiiJun <137049458+AiiJun@users.noreply.github.com>
2023-10-14 18:54:53 +0800

    Pinia-storeToRefs结构响应式数据

--- Vue3/vue-demo/src/App.vue
+++ Vue3/vue-demo/src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { storeToRefs } from 'pinia';
 import Son1Com from '@/components/son1-com.vue'
 import Son2Com from '@/components/son2-com.vue'
 import { useCounterStore } from '@/store/counter'
@@ -6,17 +7,22 @@ import { useChannelStore } from '@/store/channel'
 const counterStore = useCounterStore()
 const channelStore = useChannelStore()
 // console.log(counterStore)
+
+// 此时, 直接结构, 不处理, 数据会丢失响应式
+const { count, msg } = storeToRefs(counterStore)
+const { channelList } = storeToRefs(channelStore)
+const { getList } = channelStore
 </script>
 
 <template>
   <div>
-    <h3>App.vue根组件 - {{ counterStore.msg }} - {{ counterStore.count }}</h3>
+    <h3>App.vue根组件 - {{ msg }} - {{ count }}</h3>
     <Son1Com></Son1Com>
     <Son2Com></Son2Com>
     <hr>
-    <button @click="channelStore.getList">获取频道数据</button>
+    <button @click="getList">获取频道数据</button>
     <ul>
-      <li v-for="item in channelStore.channelList" :key="item.id">{{ item.name }}</li>
+      <li v-for="item in channelList" :key="item.id">{{ item.name }}</li>
     </ul>
   </div>
 </template>
